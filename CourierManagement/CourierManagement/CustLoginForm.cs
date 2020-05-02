fcf9b1091d773e276bba00c772aa4845069a45bf
Shakhawat118 <62530339+Shakhawat118@users.noreply.github.com>
2020-05-02 16:42:26 +0600

    link form

--- CourierManagement/CourierManagement/CustLoginForm.cs
+++ CourierManagement/CourierManagement/CustLoginForm.cs
@@ -19,8 +19,8 @@ namespace CourierManagement
 
         private void label4_Click(object sender, EventArgs e)
         {
-            CustHomeForm a = new CustHomeForm();
-            a.ShowDialog();
+            CustHomeForm h = new CustHomeForm();
+            h.ShowDialog();
         }
 
         private void LoginForm_Load(object sender, EventArgs e)

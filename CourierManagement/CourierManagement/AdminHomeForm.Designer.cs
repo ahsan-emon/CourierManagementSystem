6f3e39e242cf73fb1d8227ca4569e1354bfceacb
ahsan-emon <ahsan.emon23@gmail.com>
2020-05-06 22:57:16 +0600

    Admin form modified

--- CourierManagement/CourierManagement/AdminHomeForm.Designer.cs
+++ CourierManagement/CourierManagement/AdminHomeForm.Designer.cs
@@ -30,19 +30,25 @@
         {
             this.label14 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,13 +67,30 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label20);
+            this.panel1.Controls.Add(this.label19);
+            this.panel1.Controls.Add(this.label18);
+            this.panel1.Controls.Add(this.label17);
             this.panel1.Controls.Add(this.label16);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label12);
             this.panel1.Location = new System.Drawing.Point(184, 49);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(469, 393);
             this.panel1.TabIndex = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Worker List";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label12
             // 
@@ -75,11 +98,11 @@
             this.label12.BackColor = System.Drawing.Color.MintCream;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.Purple;
-            this.label12.Location = new System.Drawing.Point(201, 14);
+            this.label12.Location = new System.Drawing.Point(196, 24);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(85, 16);
+            this.label12.Size = new System.Drawing.Size(88, 16);
             this.label12.TabIndex = 5;
-            this.label12.Text = "Login From";
+            this.label12.Text = "Home From";
             // 
             // label10
             // 
@@ -87,11 +110,11 @@
             this.label10.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.label10.Location = new System.Drawing.Point(36, 190);
+            this.label10.Location = new System.Drawing.Point(63, 190);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(114, 20);
+            this.label10.Size = new System.Drawing.Size(55, 20);
             this.label10.TabIndex = 49;
-            this.label10.Text = "Login/Logout";
+            this.label10.Text = "Name";
             // 
             // label6
             // 
@@ -114,11 +137,11 @@
             this.label13.ForeColor = System.Drawing.Color.Cornsilk;
             this.label13.Image = global::CourierManagement.Properties.Resources.delete;
             this.label13.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label13.Location = new System.Drawing.Point(13, 379);
+            this.label13.Location = new System.Drawing.Point(11, 317);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(165, 29);
+            this.label13.Size = new System.Drawing.Size(167, 29);
             this.label13.TabIndex = 53;
-            this.label13.Text = "     Delete Acc.";
+            this.label13.Text = "     Del. Branch";
             // 
             // label11
             // 
@@ -127,13 +150,24 @@
             this.label11.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.Cornsilk;
-            this.label11.Image = global::CourierManagement.Properties.Resources.service_history;
             this.label11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label11.Location = new System.Drawing.Point(11, 286);
+            this.label11.Location = new System.Drawing.Point(9, 410);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(167, 29);
+            this.label11.Size = new System.Drawing.Size(169, 29);
             this.label11.TabIndex = 52;
-            this.label11.Text = "     Ser. History";
+            this.label11.Text = "                          ";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Image = global::CourierManagement.Properties.Resources.remove_Emp;
+            this.label17.Location = new System.Drawing.Point(331, 92);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(78, 69);
+            this.label17.TabIndex = 13;
+            this.label17.Text = "   ";
             // 
             // label3
             // 
@@ -142,25 +176,48 @@
             this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Cornsilk;
-            this.label3.Image = global::CourierManagement.Properties.Resources.edit_profile1;
             this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label3.Location = new System.Drawing.Point(11, 348);
+            this.label3.Location = new System.Drawing.Point(9, 379);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(167, 29);
+            this.label3.Size = new System.Drawing.Size(169, 29);
             this.label3.TabIndex = 51;
-            this.label3.Text = "     Edit Profile ";
+            this.label3.Text = "                          ";
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.BackColor = System.Drawing.Color.White;
-            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Image = global::CourierManagement.Properties.Resources.add_Emp;
-            this.label16.Location = new System.Drawing.Point(69, 96);
+            this.label16.BackColor = System.Drawing.Color.BurlyWood;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Image = global::CourierManagement.Properties.Resources.emp_add;
+            this.label16.Location = new System.Drawing.Point(203, 92);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(123, 91);
-            this.label16.TabIndex = 7;
-            this.label16.Text = "    ";
+            this.label16.Size = new System.Drawing.Size(78, 69);
+            this.label16.TabIndex = 12;
+            this.label16.Text = "   ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.YellowGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = global::CourierManagement.Properties.Resources.position_emp;
+            this.label2.Location = new System.Drawing.Point(203, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 69);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "   ";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Beige;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Image = global::CourierManagement.Properties.Resources.memberList_5121;
+            this.label15.Location = new System.Drawing.Point(72, 92);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(78, 69);
+            this.label15.TabIndex = 10;
+            this.label15.Text = "   ";
             // 
             // label9
             // 
@@ -171,7 +228,7 @@
             this.label9.ForeColor = System.Drawing.Color.Cornsilk;
             this.label9.Image = global::CourierManagement.Properties.Resources.Setting;
             this.label9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label9.Location = new System.Drawing.Point(12, 317);
+            this.label9.Location = new System.Drawing.Point(12, 286);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(166, 29);
             this.label9.TabIndex = 48;
@@ -186,7 +243,7 @@
             this.label8.ForeColor = System.Drawing.Color.Cornsilk;
             this.label8.Image = global::CourierManagement.Properties.Resources.logOut11;
             this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label8.Location = new System.Drawing.Point(13, 413);
+            this.label8.Location = new System.Drawing.Point(13, 348);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(165, 29);
             this.label8.TabIndex = 47;
@@ -213,13 +270,13 @@
             this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Cornsilk;
-            this.label5.Image = global::CourierManagement.Properties.Resources.tracking;
+            this.label5.Image = global::CourierManagement.Properties.Resources.add_Branch;
             this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label5.Location = new System.Drawing.Point(11, 255);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(167, 29);
             this.label5.TabIndex = 44;
-            this.label5.Text = "    Track Order";
+            this.label5.Text = "     Add Branch";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
@@ -237,16 +294,35 @@
             this.label4.TabIndex = 43;
             this.label4.Text = "         Home      ";
             // 
-            // label1
+            // label18
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(75, 191);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 17);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "New Employee";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(205, 172);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(74, 13);
+            this.label18.TabIndex = 14;
+            this.label18.Text = "Add Worker";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(322, 172);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(98, 13);
+            this.label19.TabIndex = 15;
+            this.label19.Text = "Remove Worker";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(172, 311);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(133, 13);
+            this.label20.TabIndex = 16;
+            this.label20.Text = "Solve Worker Problem";
             // 
             // AdminHomeForm
             // 
@@ -257,9 +333,9 @@
             this.Controls.Add(this.label14);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label10);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -283,7 +359,6 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
@@ -293,5 +368,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label20;
     }
 }

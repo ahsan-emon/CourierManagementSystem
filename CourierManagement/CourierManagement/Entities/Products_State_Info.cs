d09c195089496c85f5daa55531c80da5d3dbf952
nursm86 <lucifersm86@gmail.com>
2020-05-06 22:12:03 +0600

    New Class Added

---
+++ CourierManagement/CourierManagement/Entities/Products_State_Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierManagement.Entities
+{
+    class Products_State_Info : BaseEntity
+    {
+        public enum ProductStateEnum
+        {
+            Received,
+            Shipped,
+
+        }
+        public int Product_State { get; set; }
+        public DateTime Release_Date { get; set; }
+    }
+}
